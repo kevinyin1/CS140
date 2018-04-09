e9ceabf649fc5ef31c6b67deabcea6e094c6333d
Kevin Yin <kevin.yin3@gmail.com>
2018-04-08 23:10:40 -0400

    update assignment07

--- assignment07/src/assignment07/SubSetGenerator.java
+++ assignment07/src/assignment07/SubSetGenerator.java
@@ -6,52 +6,32 @@ import java.util.List;
 public class SubSetGenerator {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		list = subSet("kevin");
-		System.out.println(list);
+		System.out.println(subSet("app"));
 	}
 	
 	public static List<String> subSet(String base) {
-		List<String> list = subSetHelper(base, base.length(), base.length(), new ArrayList<String>());
-		List<String> rlist = new ArrayList<String>();
-		int n = base.length(), pos = 0, index = 0;
-		String str;
-		//organize list by size
-		while(n >= 0) {
-			str = list.get(pos);
-			if(str.length() == n) rlist.add(str);
-			pos++;
-			if(pos == list.size()) {
-				n--;
-				pos = 0;
-			}
-		}
-		pos = 0;
-		//bring all the starting character of base to the front
-		while(pos < rlist.size()) {
-			str = rlist.get(pos);
-			if(str.startsWith(base.substring(0, 1))) {
-				rlist.remove(str);
-				rlist.add(index, str);
-				index++;
-			}
-			pos++;
-		}
-		System.out.println(list);
-		return rlist;
+		List<String> list = new ArrayList<String>();
+		list.add(""); // default value
+		return subSetHelper(base, list);
 	}
-	
-	public static List<String> subSetHelper(String base, int index, int delta, List<String> list) {
-		if(delta == -1) return list;
-		String str = base.substring(0, delta) + base.substring(index);
-		if(delta == 1) { // add all the individual characters in the string
-			String character = base.substring(delta, index);
-			if(!list.contains(character)) list.add(0, character); 
-		}
-		if(!list.contains(str)) list.add(str);
-		if(index - delta == 0) return subSetHelper(base, base.length(), delta - 1, list);
-		return subSetHelper(base, index - 1, delta, list);
+	// String base = rum;
+	//									   ""
+	//								   /        \  
+	//			add "r"			  "r"	            ""
+	//							 /   \	         /       \	
+	//			add "u"		  "ru"    "r"       "u"       ""
+	//                        /   \     \       /  \      /    \ 
+	//          add "m"    "rum"  "ru"  "rm"  "um"  "u" "m"     ""
+	//
+	public static List<String> subSetHelper(String base, List<String> list) {
+		if(base.length() == 0) return list;
+		List<String> clone = (List<String>) ((ArrayList<String>) list).clone();
+		list.set(0, list.get(0) + base.substring(0, 1));
+		subSetHelper(base.substring(1), list);
+		List<String> temp = subSetHelper(base.substring(1), clone);
+		list.removeAll(temp); //remove the duplicates from the original array
+		list.addAll(temp); //add the clone values
+		return list;
 	}
-	
 	
 }
--- assignment07/src/assignment07/SubstringGenerator.java
+++ assignment07/src/assignment07/SubstringGenerator.java
@@ -8,46 +8,41 @@ import java.util.ArrayList;
 public class SubstringGenerator {
 
 	public static void main(String[] args) {
-		Set<String> s = new TreeSet<String>();
-		s = subStrings("rum");
-		System.out.println(s);
-		List<String> a = new ArrayList<String>();
-		a = subStrings1("rum");
-		System.out.println(a);
+//		System.out.println(subStrings("rum"));
+//		System.out.println(subStrings1("rum"));
 	}
 	
 	public static Set<String> subStrings(String base) {
-		Set<String> s = new TreeSet<String>();
-		return subStringsHelper(base, 1, base.length(), s);
-	}
-	
-	public static Set<String> subStringsHelper(String base, int index,int base_index, Set<String> s) {
+		Set<String> set = new TreeSet<String>();
 		if(base.length() == 0) {
-			s.add(" ");
-			return s;
+			set.add(base);
+			return set;
 		}
-		s.add(base.substring(0, index));
-		if(base_index == index) {
-			return subStringsHelper(base.substring(1), 1, base_index - 1, s);
-		}
-		return subStringsHelper(base, index + 1, base_index, s);
+		set.addAll(subStrings(base.substring(0, base.length() - 1)));
+		set.add(base);
+		set.addAll(subStrings(base.substring(1)));
+		return set;
 	}
 	
 	public static List<String> subStrings1(String base) {
-		ArrayList<String> s = new ArrayList<String>();
-		return subStrings1Helper(base, 1, base.length(), s);
-	}
-
-	public static List<String> subStrings1Helper(String base, int index, int base_index, ArrayList<String> s) {
+		List<String> list = new ArrayList<String>();
 		if(base.length() == 0) {
-			s.add("");
-			return s;
+			list.add("");
+			return list;
 		}
-		s.add(base.substring(0, index));
-		if(base_index == index) {
-			return subStrings1Helper(base.substring(1), 1, base_index - 1, s);
-		}
-		return subStrings1Helper(base, index + 1, base_index, s);
+		list.addAll(subStringsHelper(base));
+		list.addAll(subStrings1(base.substring(1)));
+		return list;
 	}
 	
+	public static List<String> subStringsHelper(String base) {
+		List<String> temp = new ArrayList<String>();
+		if(base.length() == 1) {
+			temp.add(base);
+			return temp;
+		}
+		temp.addAll(subStringsHelper(base.substring(0, base.length() - 1)));
+		temp.add(base);
+		return temp;
+	}
 }
